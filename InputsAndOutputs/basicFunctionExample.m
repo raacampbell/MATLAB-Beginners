8561db59ebec99ae3d154e350e94e66e0fd540c1
Rob Campbell <git@raacampbell.com>
2015-12-02 21:12:46 +0100

    simplify basicFunctionExample even more

--- InputsAndOutputs/basicFunctionExample.m
+++ InputsAndOutputs/basicFunctionExample.m
@@ -1,11 +1,4 @@
 function result = basicFunctionExample(inputA,inputB)
-if nargin<2
-	error('Two input arguments needed')
-end
-if ~isnumeric(inputA) | ~isnumeric(inputB)
-	error('Two numeric inputs needed')
-end
-if length(inputA) ~= 1 |  length(inputB) ~= 1
-	error('Inputs should have a length of 1')
-end
+
+
 result = inputA * inputB;
